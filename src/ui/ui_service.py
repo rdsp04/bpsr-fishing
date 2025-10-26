0ffd874613d688c7248995f5c505a4ff4c5ab8a6
Ladislav Král <ethxrnity@gmail.com>
2025-10-27 00:35:02 +0100

    Design update

--- src/ui/ui_service.py
+++ src/ui/ui_service.py
@@ -1,19 +1,14 @@
 import webview
-from pathlib import Path
-from src.utils.path import get_data_dir
-from src.ui.stats_api import StatsApi
-from src.ui.overview_api import OverviewApi
 
-BASE = get_data_dir()
-HTML_PATH = BASE / "html"
-
-class Window:
-    STATS = "stats"
-    OVERLAY = "overlay"
-
-windows = {}
 
 def start_ui():
+    from src.ui.stats_api import StatsApi
+    from src.ui.overview_api import OverviewApi
+    from src.utils.path import get_data_dir
+
+    BASE = get_data_dir()
+    HTML_PATH = BASE / "html"
+
     api = StatsApi()
     overview_api = OverviewApi()
 
@@ -22,39 +17,31 @@ def start_ui():
     with open(HTML_PATH / "main.html", "r", encoding="utf-8") as f:
         main_html = f.read()
 
-    windows[Window.STATS] = webview.create_window(
+    main_window = webview.create_window(
         "bpsr-fishing Stats",
         html=main_html,
         js_api=api,
-        width=800,
-        height=600,
+        width=1200,
+        height=700,
         min_size=(400, 300),
         resizable=True,
         frameless=False,
-        transparent=True,
+        transparent=False,
         minimized=True,
     )
 
-    windows[Window.OVERLAY] = webview.create_window(
-        "bpsr-fishing Overlay",
-        html=overlay_html,
-        js_api=overview_api,
-        width=300,
-        height=150,
-        resizable=True,
-        frameless=True,
-        transparent=True,
-        on_top=True,
-        x=0,
-        y=0
-    )
-
-    webview.start(debug=False, http_server=False)
+    overlay_window = webview.create_window(
+    "bpsr-fishing Overlay",
+    html=overlay_html,
+    js_api=overview_api,
+    frameless=True,
+    transparent=False,
+    easy_drag=True,
+    width=310,
+    height=170,
+    resizable=False,
+    on_top=True
+)
 
 
-def get_window(window_enum: str):
-    return windows.get(window_enum)
-
-
-if __name__ == "__main__":
-    start_ui()
+    webview.start()
